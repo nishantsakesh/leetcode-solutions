1891dbb255a6e5dbd6c52d1b664d6cd56411c3f1
Nishant Sakesh <91001667+nishantsakesh@users.noreply.github.com>
2026-09-08 23:50:49 +0530

    Time: 16 ms (88.42%), Space: 48.3 MB (39.63%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int maxLength = 0;
+
+        for(int num: nums){
+            countMap.put(num, countMap.getOrDefault(num, 0)+1);
+        }
+
+        for(int key: countMap.keySet()){
+            if(countMap.containsKey(key + 1)){
+                int currentLength = countMap.get(key) + countMap.get(key + 1);
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+        return maxLength;
+    }
+}
